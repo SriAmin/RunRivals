ce75b0d7c26712e5533bb820c91dd7e588bc4e18
Sri Amin <sri.amin@gmail.com>
2020-03-12 19:58:41 -0400

    Add StackNavigator into the project

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const Hello = () => {
+    return <View>
+        <Text>Open up App.js to start working on your app!</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default Hello
