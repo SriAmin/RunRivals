105922a10b6930ff6eb73cdde57bc3c554364d23
Sri Amin <sri.amin@gmail.com>
2020-03-16 15:32:53 -0400

    Added ability to obtain data from Facebook Account

--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,38 @@
 import React, {useState} from 'react'
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native'
+import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native'
 import { AuthSession } from 'expo';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as Facebook from 'expo-facebook';
 
 const FB_APP_ID = '712634022836325';
 
 const Login = () => {
 
     const [result, setResult] = useState(null);
-    const handleRequest = async () => {
-        let redirectUrl = AuthSession.getRedirectUrl(); 
-            let result = await AuthSession.startAsync(
-            {
-                    authUrl:
-                    `https://www.facebook.com/v2.8/dialog/oauth?response_type=token`  +
-                    `&client_id=${FB_APP_ID}` +
-                    `&redirect_uri=${
-                    encodeURIComponent(
-                    redirectUrl
-                ) }`,
-            }
-        );
-        setResult(result);
-    }
+
+    async function logIn() {
+        try {
+          await Facebook.initializeAsync('712634022836325');
+          const {
+            type,
+            token
+          } = await Facebook.logInWithReadPermissionsAsync({
+            permissions: ['public_profile', 'email', 'user_friends'],
+          });
+          if (type === 'success') {
+            // Get the user's name using Facebook's Graph API
+            const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+
+            const json = await response.json();
+            console.log(json);
+            alert(`Logged In, Hello ${json.name}`);
+          } else {
+            alert("Abandoned Login!")
+          }
+        } catch ({ message }) {
+          alert(`Facebook Login Error: ${message}`);
+        }
+      }
 
     return <View style={styles.pageContainer}>
             <View style={styles.inputContainer}>
@@ -33,7 +43,7 @@ const Login = () => {
                 <Text style={styles.signUp}>No Account? <Text style={{color: 'blue'}} onPress={() => {alert("Sign Up Button Pressed")}}>Sign Up </Text> </Text>
             </View>
             <View style={styles.socialContainer}>
-                <TouchableOpacity onPress={handleRequest}>
+                <TouchableOpacity onPress={logIn}>
                     <Text style={styles.fBookButton}>Log In Through Facebook</Text>
                 </TouchableOpacity>
             </View>
