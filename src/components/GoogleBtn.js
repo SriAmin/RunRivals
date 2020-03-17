631c262978688a143108e939ac8f25592a2c1d9a
Sri Amin <sri.amin@gmail.com>
2020-03-17 19:42:30 -0400

    Displayed more data from user and commented code

--- src/components/GoogleBtn.js
+++ src/components/GoogleBtn.js
@@ -3,28 +3,41 @@ import {View, Text, StyleSheet} from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import * as Google from 'expo-google-app-auth';
 
-const GoogleBtn = () => {
+const GoogleBtn = (props) => {
 
+    //iOS - 737913280141-ri24odrhv8afpam0psr6qnhub1uoopd5.apps.googleusercontent.com
+    //Android- 737913280141-4pcmk2t56rllgrdqfmninh5t6blfljh9.apps.googleusercontent.com
+
+    //Using Google API Credentials, we can log in and obtain user data
     async function signInWithGoogleAsync() {
         try {
+            //Performs the request to sign in
             const result = await Google.logInAsync({
+                expoClientId: "737913280141-ha4lp7pabipqb1prqeneap6mso7fqrda.apps.googleusercontent.com",
                 iosClientId: "737913280141-ri24odrhv8afpam0psr6qnhub1uoopd5.apps.googleusercontent.com",
                 androidClientId: "737913280141-4pcmk2t56rllgrdqfmninh5t6blfljh9.apps.googleusercontent.com",
                 scopes: ['profile', 'email'],
             });
             console.log(result)
+            //If login was successfull, it'll display user information and display profile picture
             if (result.type == "success"){
-                //console.log(result);
+                const user = result.user
+                alert(`Logged In! \n Hello ${user.name}\n Email: ${user.email}`)
+                props.urlSetter(user.photoUrl);
                 return result.accessToken;
-            } 
+            }
+            //If user failed to log in 
             else {
                 alert("Abonded Login");
             }
-        } catch (e) {
-            alert(e)
+        }
+        //Catch any crash worthy error throughout the process 
+        catch (e) {
+            alert(`Google Login Failure: ${e}`)
         }
     }
 
+    //Displays the Google Sign in Button
     return <View>
         <TouchableOpacity onPress={signInWithGoogleAsync}>
             <Text style={styles.googleButton}>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,32 +4,9 @@ import FacebookBtn from '../components/FacebookBtn'
 import GoogleBtn from '../components/GoogleBtn'
 import * as Google from 'expo-google-app-auth';
 
-//iOS - 737913280141-ri24odrhv8afpam0psr6qnhub1uoopd5.apps.googleusercontent.com
-//Android- 737913280141-4pcmk2t56rllgrdqfmninh5t6blfljh9.apps.googleusercontent.com
-
 const Login = () => {
     //State used for profile image obtained from social authentication (Google, Facebook)
     let [url, setURL] = useState("#");
-
-    async function signInWithGoogleAsync() {
-        try {
-            const result = await Google.logInAsync({
-                iosClientId: "737913280141-ri24odrhv8afpam0psr6qnhub1uoopd5.apps.googleusercontent.com",
-                androidClientId: "737913280141-4pcmk2t56rllgrdqfmninh5t6blfljh9.apps.googleusercontent.com",
-                scopes: ['profile', 'email'],
-            });
-            console.log(result)
-            if (result.type == "success"){
-                //console.log(result);
-                return result.accessToken;
-            } 
-            else {
-                alert("Abonded Login");
-            }
-        } catch (e) {
-            alert(e)
-        }
-    }
 
     //Represents the main login page with text fields and buttons for social authentication
     return <View style={styles.pageContainer}>
@@ -42,7 +19,7 @@ const Login = () => {
                 <Image style={styles.imageStyle} source={{uri: url}} />
             </View>
             <View style={styles.socialContainer}>
-                <GoogleBtn />
+                <GoogleBtn urlSetter={setURL}/>
                 <FacebookBtn urlSetter={setURL}/>
             </View>
     </View>
